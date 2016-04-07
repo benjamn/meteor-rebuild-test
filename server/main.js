0607343569fc7f7ed98a2839a31e88b00c8f9548
Ben Newman <bn@cs.stanford.edu>
2016-04-06 20:42:45 -0400

    Reuse salt between calls to computeHash.

--- server/main.js
+++ server/main.js
@@ -1,12 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import bcrypt from "bcrypt";
 
-const genSalt = Promise.denodeify(bcrypt.genSalt);
+const salt = Promise.denodeify(bcrypt.genSalt)(10);
 const hash = Promise.denodeify(bcrypt.hash);
 
 Meteor.methods({
   computeHash(str) {
-    const salt = genSalt(10).await();
-    return hash(str, salt).await();
+    return hash(str, salt.await()).await();
   }
 });
